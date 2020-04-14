2cb10961d5b294c51edfea95a80905adc3ed8438
Soumyadeep Sinha <s.sinha2701@gmail.com>
2020-04-14 08:03:02 +0000

    dbconnection.php edited online with Bitbucket

--- dbconnection.php
+++ dbconnection.php
@@ -3,13 +3,12 @@
 $server = 'localhost';
 $user = 'root';
 $password = '';
-$db = 'databasename';     // Enter your db name
+$db = 'databasename';
+
+// Attempt to connect to MySQL database
+$con = mysqli_connect($server, $user, $password, $db);
 
 // Check connection
 if ($con === false) {
   die("ERROR: Could not connect. " . mysqli_connect_error());
-} else {
-// Attempt to connect to MySQL database
-$con = mysqli_connect($server, $user, $password, $db);
 }
-
