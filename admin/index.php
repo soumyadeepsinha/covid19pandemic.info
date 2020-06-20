5b399bd7a99435b9e6f666304a12c901197497a7
Soumyadeep Sinha <52245063+soumyadeepsinha@users.noreply.github.com>
2020-06-20 12:55:44 +0530

    Update Index page for Admin Login

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,56 @@
 <?php
-$url = "https://www.mohfw.gov.in/";
-$html = file_get_contents($url);
-$dom = new domDocument;
-@$dom->loadHTML($html);
-$tables = $dom->getElementsByTagName('table');
-$rows = $tables->item(0)->getElementsByTagName('tr');
+require_once '../dbconnection.php';
+session_start();
 
-$total = '';
-$active = '';
-$confirmed = '';
-$discharged = '';
-$death = '';
-foreach ($rows as $row) {
-  $cols = $row->getElementsByTagName('td');
-  if (isset($cols->item(0)->nodeValue) && isset($cols->item(1)->nodeValue) && isset($cols->item(2)->nodeValue) && isset($cols->item(3)->nodeValue) && isset($cols->item(4)->nodeValue) && isset($cols->item(5)->nodeValue)) {
-    $confirmed = $cols->item(5)->nodeValue;
-    $active = $cols->item(2)->nodeValue;
-    $discharged = $cols->item(3)->nodeValue;
-    $death = $cols->item(4)->nodeValue;
+$php_errormsg = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // declearing variable & value
+  $myusername = mysqli_real_escape_string($con, $_POST['username']);
+  $mypassword = mysqli_real_escape_string($con, $_POST['password']);
+  // sql query for retrieving username & password from database
+  $sql = "SELECT * FROM administrator WHERE user = '$myusername' and password = '$mypassword'";
+  $result = mysqli_query($con, $sql);
+
+  if (mysqli_num_rows($result) == 1) {
+    $_SESSION['user'] = $myusername;
+    // redirect to user details page
+    header('location: userdetails.php');
+  } else {
+    $php_errormsg = "Your Login Name or Password is invalid";
+    header('location: index.php');
   }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Remove Favicon -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <!-- Custom stylesheet -->
+  <link rel="stylesheet" href="assets/style.css">
+  <title>Admin | Login</title>
+</head>
+
+<body>
+  <div class="main">
+    <form action="" method="post">
+      <p class="sign" align="center">Sign in</p>
+      <form class="form1">
+        <input class="un " type="text" placeholder="Username" name="username" autocomplete="off" required />
+        <input class="pass" type="password" placeholder="Password" name="password" autocomplete="off" required />
+        <input type="submit" class="btn submit" value="Submit" />
+        <p class="forgot" align="center"><a href="#">Forgot Password?</p>
+      </form>
+      <div style="font-size:14px; color:red; margin-top:10px"><?php echo $php_errormsg; ?></div>
+  </div>
+</body>
+
+</html>
